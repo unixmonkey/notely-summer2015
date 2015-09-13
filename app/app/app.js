0561fb796c14781b01f72da9c686eecb9e230531
David Jones <unixmonkey1@gmail.com>
2015-09-13 11:42:44 -0400

    Move focusOn attribute directive to app/components

--- app/app/app.js
+++ app/app/app.js
@@ -1,15 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app = angular.module('notely', [
+angular.module('notely', [
   'ngCookies',
   'notely.notes'
 ]);
-
-app.directive('focusOn', function() {
-  return function(scope, elem, attr) {
-    scope.$on(attr.focusOn, function(e) {
-      elem[0].focus();
-    });
-  };
-});
---
+++ app/app/directives/focus-on.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('notely')
+.directive('focusOn', function() {
+  return function(scope, elem, attr) {
+    scope.$on(attr.focusOn, function(e) {
+      elem[0].focus();
+    });
+  };
+});
