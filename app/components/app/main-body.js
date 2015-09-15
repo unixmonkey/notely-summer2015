5fa0ca70a29ce981ba97b089b70a347387c4837d
David Jones <unixmonkey1@gmail.com>
2015-09-15 18:35:55 -0400

    Make top-level mainBody component

---
+++ app/components/app/main-body.js
@@ -0,0 +1,16 @@
+import mainHeader from './main-header';
+import mainContainer from './main-container';
+
+angular.module('notely')
+.directive('mainBody', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {},
+    template: `
+      <div class="container wrap">
+        <main-header></main-header>
+        <main-container></main-container>
+      </div>`
+  };
+});
--- app/index.js
+++ app/index.js
@@ -3,5 +3,4 @@ require('./services/notes-backend.js');
 require('./services/current-user.js');
 require('./services/current-note.js');
 require('./directives/focus-on.js');
-require('./components/app/main-container.js');
-require('./components/app/main-header.js');
+require('./components/app/main-body.js');
